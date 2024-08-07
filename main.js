22e0f199cb624f15048cba2822acb1f540643bd9
MUHAMMADHAMMAD6623 <142398205+MUHAMMADHAMMAD6623@users.noreply.github.com>
2024-08-07 23:07:26 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const hamico = document.querySelector('.menu')
-const rightsec = document.querySelector('.right')
+const hamico = document.querySelector('.menu');
+const rightsec = document.querySelector('.right');
+const universe = document.querySelector('body')
 
-hamico.addEventListener('click',()=>{
-    hamico.classList.toggle('hamclicked')
-})
+hamico.addEventListener('click', () => {
+    hamico.classList.toggle('hamclicked');
+    universe.classList.toggle('not-needed')
+    if (rightsec.style.height === '0px' || rightsec.style.height === '') {
+        rightsec.style.height = '100vh';
+    } else {
+        rightsec.style.height = '0px';
+    }
+});
